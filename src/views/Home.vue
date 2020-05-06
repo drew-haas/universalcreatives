7eb39e9edf062ee6d97646df7dbb0d602dd28ebb
Andrew Haas <ahaas3@apple.com>
2020-05-06 10:22:34 -0700

    update styles remove default  stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/uc-logo-v1.png">
+    <!-- <p>I'm baby brooklyn paleo pork belly, chicharrones live-edge mumblecore vegan man bun. Williamsburg actually hoodie sriracha. Portland mumblecore affogato 8-bit, vaporware poutine helvetica brunch narwhal vinyl shoreditch kale chips. Gluten-free beard jianbing butcher meditation four dollar toast. Sartorial put a bird on it bushwick lomo celiac polaroid. Pop-up lyft kitsch banjo cronut tousled YOLO iPhone cliche.</p> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
+
+<style lang="scss">
+.home {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+
+  &.dark-mode {
+    filter: invert(100%);
+  }
+
+  img {
+    max-width: 200px;
+    transform: translateX(-17px);
+  }
+
+  p {
+    max-width: 350px;
+    text-align: justify;
+    margin-top: 50px;
+    font-weight: bold;
+    line-height: 1.7;
+  }
+}
+</style>
